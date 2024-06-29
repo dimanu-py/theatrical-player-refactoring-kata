e2247cf0de5d181ccc9861b0e36be70d1e2cc685
dimanu-py <diegomtz126@gmail.com>
2024-06-29 16:20:02 +0200

    refactor(statement): remove use of total_amount and this_amount

--- src/theatrical_player/statement.py
+++ src/theatrical_player/statement.py
@@ -5,34 +5,25 @@ from src.theatrical_player.money import Money
 
 
 def statement_printer(invoice, plays):
-    total_amount = 0
-    invoice_amount = Money(initial_amount=total_amount)
+    invoice_amount = Money(initial_amount=0)
     invoice_credits = Credits(initial_credits=0)
     result = f'Statement for {invoice["customer"]}\n'
-
-    def format_as_dollars(amount):
-        return f"${amount:0,.2f}"
 
     for perf in invoice['performances']:
         play = plays[perf['playID']]
         if play['type'] == "tragedy":
-            this_amount = 40000
-            performance_amount = Money(this_amount)
+            performance_amount = Money(40000)
             if perf['audience'] > 30:
-                extra_amount_by_audience = 1000 * (perf['audience'] - 30)
-                this_amount += extra_amount_by_audience
-                performance_amount = performance_amount.add(Money(extra_amount_by_audience))
+                extra_amount_by_audience = Money(1000 * (perf['audience'] - 30))
+                performance_amount = performance_amount.add(extra_amount_by_audience)
         elif play['type'] == "comedy":
-            this_amount = 30000
-            performance_amount = Money(this_amount)
+            performance_amount = Money(30000)
             if perf['audience'] > 20:
-                extra_amount_by_audience = 10000 + 500 * (perf['audience'] - 20)
-                this_amount += extra_amount_by_audience
-                performance_amount = performance_amount.add(Money(extra_amount_by_audience))
+                extra_amount_by_audience = Money(10000 + 500 * (perf['audience'] - 20))
+                performance_amount = performance_amount.add(extra_amount_by_audience)
 
-            comedy_extra_amount = 300 * perf['audience']
-            this_amount += comedy_extra_amount
-            performance_amount = performance_amount.add(Money(comedy_extra_amount))
+            comedy_extra_amount = Money(300 * perf['audience'])
+            performance_amount = performance_amount.add(comedy_extra_amount)
 
         else:
             raise ValueError(f'unknown type: {play["type"]}')
@@ -45,7 +36,6 @@ def statement_printer(invoice, plays):
             invoice_credits = invoice_credits.add(comedy_extra_credits)
         # print line for this order
         result += f' {play["name"]}: {performance_amount.as_dollar()} ({perf["audience"]} seats)\n'
-        total_amount += this_amount
         invoice_amount = invoice_amount.add(performance_amount)
 
     result += f'Amount owed is {invoice_amount.as_dollar()}\n'
