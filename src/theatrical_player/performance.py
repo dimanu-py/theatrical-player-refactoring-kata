b0d68f660281f7f1c4f3f1dc7bb4e05970106aa2
dimanu-py <diegomtz126@gmail.com>
2024-06-30 00:17:32 +0200

    feat(performance): make PerformanceRepository iterable

--- src/theatrical_player/performance.py
+++ src/theatrical_player/performance.py
@@ -1,3 +1,6 @@
+from typing import Iterable
+
+
 class Performance:
 
     def __init__(self, play_id: str, audience: int) -> None:
@@ -9,3 +12,6 @@ class PerformancesRepository:
 
     def __init__(self, performances: list[Performance]) -> None:
         self.performances = performances
+
+    def __iter__(self) -> Iterable[Performance]:
+        return iter(self.performances)
