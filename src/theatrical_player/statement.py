d675eddbb97470ea158ae9380979a8f2069633ab
dimanu-py <diegomtz126@gmail.com>
2024-06-29 21:49:45 +0200

    refactor(statement): pass print method a dictionary of Play objects instead of raw dictionary

--- src/theatrical_player/statement.py
+++ src/theatrical_player/statement.py
@@ -11,21 +11,20 @@ class StatementPrinter:
         self.invoice_money = Money(initial_amount=0)
         self.invoice_credits = Credits(initial_credits=0)
 
-    def print(self, invoice, plays):
+    def print(self, invoice, plays: dict[str, Play]):
         result = f'Statement for {invoice["customer"]}\n'
 
         for perf in invoice['performances']:
             play = plays[perf['playID']]
-            performance_play = Play(name=play["name"], genre=play["type"])
 
-            performance_amount = self.compute_performance_amount(perf, performance_play)
-            performance_credits = self.compute_performance_credits(perf, performance_play)
+            performance_amount = self.compute_performance_amount(perf, play)
+            performance_credits = self.compute_performance_credits(perf, play)
 
             self.invoice_credits = self.invoice_credits.add(performance_credits)
             self.invoice_money = self.invoice_money.add(performance_amount)
 
             # print line for this order
-            result += f' {performance_play.name}: {performance_amount.as_dollar()} ({perf["audience"]} seats)\n'
+            result += f' {play.name}: {performance_amount.as_dollar()} ({perf["audience"]} seats)\n'
 
         result += f'Amount owed is {self.invoice_money.as_dollar()}\n'
         result += f'You earned {self.invoice_credits} credits\n'
--- tests/test_statement.py
+++ tests/test_statement.py
@@ -3,6 +3,7 @@ import unittest
 
 from approvaltests import verify
 
+from src.theatrical_player.play import Play
 from src.theatrical_player.statement import StatementPrinter
 from tests.conftest import IntelliJDiffReporter
 
@@ -20,7 +21,11 @@ class TestStatement(unittest.TestCase):
 
     def test_can_produce_invoice_with_valid_plays(self):
         invoice = open_json_at("tests/test_files/invoice.json")
-        plays = open_json_at("tests/test_files/plays.json")
+        plays = {
+            "hamlet": Play("Hamlet", "tragedy"),
+            "as-like": Play("As You Like It", "comedy"),
+            "othello": Play("Othello", "tragedy")
+        }
 
         verify(
             self.statement_printer.print(invoice, plays),
@@ -29,7 +34,10 @@ class TestStatement(unittest.TestCase):
 
     def test_cannot_produce_invoice_of_unknown_plays_type(self):
         invoice = open_json_at("tests/test_files/invoice_new_plays.json")
-        plays = open_json_at("tests/test_files/new_plays.json")
+        plays = {
+            "henry-v": Play("Henry V", "history"),
+            "as-like": Play("As You Like It", "pastoral")
+        }
 
         with self.assertRaises(ValueError) as exception_info:
             self.statement_printer.print(invoice, plays)
