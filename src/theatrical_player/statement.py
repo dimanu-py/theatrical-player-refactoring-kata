81e6b69a8ba4b7dd4ea10df40059a4620d9c6182
dimanu-py <diegomtz126@gmail.com>
2024-06-29 13:59:38 +0200

    refactor(statement): rename _credits to invoice_credits

--- src/theatrical_player/statement.py
+++ src/theatrical_player/statement.py
@@ -5,7 +5,7 @@ from src.theatrical_player.credits import Credits
 
 def statement_printer(invoice, plays):
     total_amount = 0
-    _credits = Credits(initial_credits=0)
+    invoice_credits = Credits(initial_credits=0)
     result = f'Statement for {invoice["customer"]}\n'
 
     def format_as_dollars(amount):
@@ -28,16 +28,16 @@ def statement_printer(invoice, plays):
             raise ValueError(f'unknown type: {play["type"]}')
 
         extra_credits_by_audience = Credits(max(perf['audience'] - 30, 0))
-        _credits = _credits.add(extra_credits_by_audience)
+        invoice_credits = invoice_credits.add(extra_credits_by_audience)
 
         if "comedy" == play["type"]:
             comedy_extra_credits = Credits(math.floor(perf['audience'] / 5))
-            _credits = _credits.add(comedy_extra_credits)
+            invoice_credits = invoice_credits.add(comedy_extra_credits)
         # print line for this order
         result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
         total_amount += this_amount
 
     result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
-    result += f'You earned {_credits} credits\n'
+    result += f'You earned {invoice_credits} credits\n'
     return result
 
