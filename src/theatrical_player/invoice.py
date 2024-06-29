90d674ff70c0414ec37b7a1e6c90a3dd4ca1f57c
dimanu-py <diegomtz126@gmail.com>
2024-06-30 00:18:16 +0200

    refactor(statement): replace use of primitive collection for PerformanceRepository

--- src/theatrical_player/invoice.py
+++ src/theatrical_player/invoice.py
@@ -1,9 +1,8 @@
-from src.theatrical_player.performance import Performance, PerformancesRepository
+from src.theatrical_player.performance import PerformancesRepository
 
 
 class Invoice:
 
-    def __init__(self, customer: str, performances: list[Performance], performances_repository: PerformancesRepository = None) -> None:
+    def __init__(self, customer: str, performances: PerformancesRepository) -> None:
         self.customer = customer
         self.performances = performances
-        self.performances_repository = performances_repository
--- tests/test_statement.py
+++ tests/test_statement.py
@@ -17,19 +17,17 @@ class TestStatement(unittest.TestCase):
         self.intellij_diff_reporter = IntelliJDiffReporter()
 
     def test_can_produce_invoice_with_valid_plays(self):
-        performances = [
+        performances = PerformancesRepository([
             Performance("hamlet", 55),
             Performance("as-like", 35),
             Performance("othello", 40)
-        ]
-        performances_repository = PerformancesRepository(performances)
+        ])
         plays = {
             "hamlet": Play("Hamlet", "tragedy"),
             "as-like": Play("As You Like It", "comedy"),
             "othello": Play("Othello", "tragedy")
         }
-
-        invoice = Invoice(customer=self.CUSTOMER, performances=performances, performances_repository=performances_repository)
+        invoice = Invoice(customer=self.CUSTOMER, performances=performances)
 
         verify(
             self.statement_printer.print(invoice, plays),
@@ -37,16 +35,15 @@ class TestStatement(unittest.TestCase):
         )
 
     def test_cannot_produce_invoice_of_unknown_plays_type(self):
-        performances = [
-            Performance("henry-v", 53),
-            Performance("as-like", 55)
-        ]
         plays = {
             "henry-v": Play("Henry V", "history"),
             "as-like": Play("As You Like It", "pastoral")
         }
-
-        invoice = Invoice(customer=self.CUSTOMER, performances=performances, performances_repository=None)
+        performances = PerformancesRepository([
+            Performance("henry-v", 53),
+            Performance("as-like", 55)
+        ])
+        invoice = Invoice(customer=self.CUSTOMER, performances=performances)
 
         with self.assertRaises(ValueError) as exception_info:
             self.statement_printer.print(invoice, plays)
