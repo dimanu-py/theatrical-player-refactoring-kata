7ed61d93a11240e7ed93b8eb05e87cef91720c30
dimanu-py <diegomtz126@gmail.com>
2024-06-29 16:48:43 +0200

    refactor(statement): extract methods to calculate extra amount for comedy and tragedy plays

--- src/theatrical_player/statement.py
+++ src/theatrical_player/statement.py
@@ -17,14 +17,10 @@ class StatementPrinter:
             play = plays[perf['playID']]
             if play['type'] == "tragedy":
                 performance_amount = Money(40000)
-                if perf['audience'] > 30:
-                    extra_amount_by_audience = Money(1000 * (perf['audience'] - 30))
-                    performance_amount = performance_amount.add(extra_amount_by_audience)
+                performance_amount = performance_amount.add(self.tragedy_extra_amount_by_audience(perf))
             elif play['type'] == "comedy":
                 performance_amount = Money(30000)
-                if perf['audience'] > 20:
-                    extra_amount_by_audience = Money(10000 + 500 * (perf['audience'] - 20))
-                    performance_amount = performance_amount.add(extra_amount_by_audience)
+                performance_amount = performance_amount.add(self.comedy_extra_amount_by_audience(perf))
 
                 comedy_extra_amount = Money(300 * perf['audience'])
                 performance_amount = performance_amount.add(comedy_extra_amount)
@@ -45,3 +41,15 @@ class StatementPrinter:
         result += f'Amount owed is {self.money.as_dollar()}\n'
         result += f'You earned {self.credits} credits\n'
         return result
+
+    @staticmethod
+    def comedy_extra_amount_by_audience(perf: dict[str, str | int]) -> Money:
+        if perf['audience'] > 20:
+            return Money(10000 + 500 * (perf['audience'] - 20))
+        return Money(0)
+
+    @staticmethod
+    def tragedy_extra_amount_by_audience(perf: dict[str, str | int]) -> Money:
+        if perf['audience'] > 30:
+            return Money(1000 * (perf['audience'] - 30))
+        return Money(0)
