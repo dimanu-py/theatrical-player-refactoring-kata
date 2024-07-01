b213dbfe6d17641ab23fbafed09864deb92835e2
dimanu-py <diegomtz126@gmail.com>
2024-07-01 19:10:12 +0200

    feat(credits): create two credits calculation strategies

--- src/theatrical_player/credits.py
+++ src/theatrical_player/credits.py
@@ -1,13 +1,36 @@
+import math
+from abc import ABC, abstractmethod
+
+
 class Credits:
 
     def __init__(self, initial_credits: int) -> None:
-        self.credits = initial_credits
+        self._credits = initial_credits
 
     def add(self, other: "Credits") -> "Credits":
-        return Credits(self.credits + other.credits)
+        return Credits(self._credits + other._credits)
 
     def __eq__(self, other: "Credits") -> bool:
-        return self.credits == other.credits
+        return self._credits == other._credits
 
     def __str__(self) -> str:
-        return f"{self.credits}"
+        return f"{self._credits}"
+
+
+class CreditsCalculationStrategy(ABC):
+
+    @abstractmethod
+    def credits(self, audience: int) -> Credits:
+        """Calculate play's credits"""
+
+
+class NoExtraCredits(CreditsCalculationStrategy):
+
+    def credits(self, audience: int) -> Credits:
+        return Credits(0)
+
+
+class AudienceBasedCredits(CreditsCalculationStrategy):
+
+    def credits(self, audience: int) -> Credits:
+        return Credits(math.floor(audience / 5))
