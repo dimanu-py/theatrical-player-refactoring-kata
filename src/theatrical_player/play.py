9c2dfc2dde5df370c88eed643ce51fa9bca63c6a
dimanu-py <diegomtz126@gmail.com>
2024-07-01 14:42:04 +0200

    refactor(play): use polymorphism to get cost and credit of a play

--- src/theatrical_player/play.py
+++ src/theatrical_player/play.py
@@ -1,10 +1,11 @@
 import math
+from abc import abstractmethod, ABC
 
 from src.theatrical_player.credits import Credits
 from src.theatrical_player.money import Money
 
 
-class Play:
+class Play(ABC):
 
     def __init__(self, name: str, genre: str) -> None:
         self.name = name
@@ -18,66 +19,47 @@ class Play:
             return Tragedy(name, genre)
         raise ValueError(f'unknown type: {genre}')
 
+    @abstractmethod
     def credits(self, audience: int) -> Credits:
-        if self.genre == "comedy":
-            comedy = Comedy(self.name, self.genre)
-            return comedy.comedy_credits(audience)
+        """Calculate play's credits"""
 
-        if self.genre == "tragedy":
-            tragedy = Tragedy(self.name, self.genre)
-            return tragedy.tragedy_credits(audience)
-
-        raise ValueError(f'unknown type: {self.genre}')
-
+    @abstractmethod
     def cost(self, audience: int) -> Money:
-        if self.genre == "tragedy":
-            tragedy = Tragedy(self.name, self.genre)
-            return tragedy.tragedy_cost(audience)
-
-        if self.genre == "comedy":
-            comedy = Comedy(self.name, self.genre)
-            return comedy.comedy_cost(audience)
-
-        raise ValueError(f'unknown type: {self.genre}')
+        """Calculate play's cost"""
 
 
 class Comedy(Play):
 
-    def comedy_credits(self, audience: int) -> Credits:
+    def credits(self, audience: int) -> Credits:
         return Credits(math.floor(audience / 5))
 
-    def comedy_cost(self, audience: int) -> Money:
+    def cost(self, audience: int) -> Money:
         performance_amount = Money(30000)
-        performance_amount = performance_amount.add(self._comedy_extra_cost_by_audience(audience))
-        performance_amount = performance_amount.add(self._comedy_extra_cost_by_genre(audience))
+        performance_amount = performance_amount.add(self._extra_cost_by_audience(audience))
+        performance_amount = performance_amount.add(self._extra_cost(audience))
         return performance_amount
 
     @staticmethod
-    def _comedy_extra_cost_by_genre(audience):
+    def _extra_cost(audience):
         return Money(300 * audience)
 
     @staticmethod
-    def _comedy_extra_cost_by_audience(audience):
+    def _extra_cost_by_audience(audience):
         return Money(10000 + 500 * (audience - 20)) if audience > 20 else Money(0)
 
 
 class Tragedy(Play):
 
-    def tragedy_credits(self, audience: int) -> Credits:
+    def credits(self, audience: int) -> Credits:
         return Credits(0)
 
-    def tragedy_cost(self, audience: int) -> Money:
+    def cost(self, audience: int) -> Money:
         performance_amount = Money(40000)
-        performance_amount = performance_amount.add(self._tragedy_extra_cost_by_audience(audience))
-        performance_amount = performance_amount.add(self._tragedy_extra_cost_by_genre())
+        performance_amount = performance_amount.add(self._extra_cost_by_audience(audience))
         return performance_amount
 
     @staticmethod
-    def _tragedy_extra_cost_by_genre() -> Money:
-        return Money(0)
-
-    @staticmethod
-    def _tragedy_extra_cost_by_audience(audience):
+    def _extra_cost_by_audience(audience):
         return Money(1000 * (audience - 30)) if audience > 30 else Money(0)
 
 
