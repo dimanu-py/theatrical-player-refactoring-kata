d0f8a30aaa6665f41958c0530bfee0f675415dfe
dimanu-py <diegomtz126@gmail.com>
2024-07-04 15:40:16 +0200

    refactor(printer): move filling behavior to invoice
    We are constantly asking the invoice for data to fill the statement. StatementPrinter act now as coordinator, it just need to coordinate the input data with how that data should be shown

--- src/theatrical_player/invoice.py
+++ src/theatrical_player/invoice.py
@@ -1,4 +1,8 @@
+from src.theatrical_player.credits import Credits
+from src.theatrical_player.money import Money
 from src.theatrical_player.performance import PerformancesRepository
+from src.theatrical_player.play import PlaysCatalog
+from src.theatrical_player.statement import Statement
 
 
 class Invoice:
@@ -6,3 +10,17 @@ class Invoice:
     def __init__(self, customer: str, performances: PerformancesRepository) -> None:
         self.customer = customer
         self.performances = performances
+        self.invoice_money: Money = Money(initial_amount=0)
+        self.invoice_credits: Credits = Credits(initial_credits=0)
+
+    def fill(self, statement: Statement, catalog: PlaysCatalog) -> None:
+        statement.fill_customer(self.customer)
+
+        for performance in self.performances:
+            play = catalog.by_id(performance.play_id)
+            self.invoice_credits = self.invoice_credits.add(performance.credits(play))
+            self.invoice_money = self.invoice_money.add(performance.cost(play))
+
+            statement.fill_performance(play.name, performance.cost(play), performance.audience)
+
+        statement.fill_invoice(self.invoice_money, self.invoice_credits)
