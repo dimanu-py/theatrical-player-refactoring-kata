16c2146a90377b2d6598cb98bdd9757e8b01072b
dimanu-py <diegomtz126@gmail.com>
2024-07-05 20:59:17 +0200

    refactor(statement): unify all fields regarding the content of statement into a single field

--- src/theatrical_player/statement.py
+++ src/theatrical_player/statement.py
@@ -17,11 +17,8 @@ class Fillable(metaclass=ABCMeta):
 class PlainTextStatement(Fillable):
 
     def __init__(self) -> None:
-        self.cost: float = 0.0
-        self.credits: int = 0
-        self.customer: str = ""
-        self.performance: list[dict[str, Any]] = []
         self.lines: str = ""
+        self.content: dict[str, Any] = {}
 
     def fill(self, tag: str, value) -> None:
         if tag == "customer":
@@ -36,27 +33,27 @@ class PlainTextStatement(Fillable):
             raise ValueError(f"Unknown tag: {tag}")
 
     def print(self) -> str:
-        self.lines += f"Statement for {self.customer}\n"
-        for performance in self.performance:
+        self.lines += f"Statement for {self.content["customer"]}\n"
+        for performance in self.content["performance"]:
             self.lines += f" {performance['name']}: {performance['cost']} ({performance['audience']} seats)\n"
-        self.lines += f"Amount owed is {self.cost}\n"
-        self.lines += f"You earned {self.credits} credits\n"
+        self.lines += f"Amount owed is {self.content["cost"]}\n"
+        self.lines += f"You earned {self.content["credits"]} credits\n"
         return self.lines
 
     def _fill_cost(self, value: Money) -> None:
-        self.cost = value.as_dollar()
+        self.content["cost"] = value.as_dollar()
 
     def _fill_credits(self, value: int) -> None:
-        self.credits = value
+        self.content["credits"] = value
 
     def _fill_customer(self, value: str) -> None:
-        self.customer = value
+        self.content["customer"] = value
 
     def _fill_performance(self, play_name: str, cost: Money, audience: int) -> None:
-        self.performance.append(
-            {
-                "name": play_name,
-                "cost": cost.as_dollar(),
-                "audience": audience
-            }
-        )
+        if "performance" not in self.content:
+            self.content["performance"] = []
+        self.content["performance"].append({
+            "name": play_name,
+            "cost": cost.as_dollar(),
+            "audience": audience
+        })
