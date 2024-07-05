88b47489b7b17d6289bfc75820ab8052150891fa
dimanu-py <diegomtz126@gmail.com>
2024-07-05 20:28:35 +0200

    refactor(performance): remove field play_id by getting the play associated with the performance when creation

--- src/theatrical_player/performance.py
+++ src/theatrical_player/performance.py
@@ -9,32 +9,30 @@ from src.theatrical_player.statement import Fillable
 class Performance:
 
     def __init__(self, play_id: str, audience: int, catalog: PlaysCatalog) -> None:
-        self.play_id = play_id
         self.audience = audience
         self.catalog = catalog
+        self.play = catalog.by_id(play_id)
 
     def credits(self) -> Credits:
         play = self.catalog.by_id(self.play_id)
         initial_credits = Credits(max(self.audience - 30, 0))
-        return initial_credits.add(play.credits(self.audience))
+        return initial_credits.add(self.play.credits(self.audience))
 
     def cost(self) -> Money:
-        play = self.catalog.by_id(self.play_id)
-        return play.cost(self.audience)
+        return self.play.cost(self.audience)
 
     def fill(self, fillable: Fillable) -> None:
         fillable.fill(
             "performance",
             {
-                "name": self._title(self.catalog),
+                "name": self._title(),
                 "cost": self.cost(),
                 "audience": self.audience
             }
         )
 
-    def _title(self, catalog: PlaysCatalog) -> str:
-        play = catalog.by_id(self.play_id)
-        return play.title()
+    def _title(self) -> str:
+        return self.play.title()
 
 
 class PerformancesRepository:
